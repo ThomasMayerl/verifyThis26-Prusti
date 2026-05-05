8fbe6c79b0ffdd60f93ff5be5ee44509074a88a6
Thomas Mayerl <tmayerl@student.ethz.ch>
2026-05-05 17:44:06 +0200

    CH1: Safety of update

--- src/challenge1/mod.rs
+++ src/challenge1/mod.rs
@@ -17,8 +17,16 @@ use prusti_contracts::*;
 //     cnt_at_least(a, h, 0) >= h && !is_h_idx(a, h+1)
 // }
 
+#[pure]
+#[trusted]
+#[ensures(result == (forall(|i: usize, j: usize| (i < a.len() && j < a.len() && i < j) ==> a[i] >= a[j])))]
+fn sorted_rev(a: &[usize]) -> bool {
+    panic!("ABSTRACT");
+}
+
 // recursive because of Prusti loop limitations
 #[requires(a.len() == n)]
+#[requires(sorted_rev(a))]
 // #[ensures(is_h_idx(a, result))]
 fn compute_rec(a: &[usize], n: usize, curr_h: usize) -> usize {
     assert!(a.len() == n);
@@ -33,6 +41,7 @@ fn compute_rec(a: &[usize], n: usize, curr_h: usize) -> usize {
 }
 
 #[requires(a.len() == n)]
+#[requires(sorted_rev(a))]
 // #[ensures(is_h_idx(a, result))]
 // NOTE: We cannot currently verify termination since Prusti does not yet support decreases annotations
 fn compute(a: &[usize], n: usize) -> usize {
@@ -40,6 +49,7 @@ fn compute(a: &[usize], n: usize) -> usize {
 }
 
 #[requires(a.len() == n)]
+#[requires(sorted_rev(a))]
 #[requires(low <= n && high <= n)]
 fn compute_opt_rec(a: &[usize], n: usize, low: usize, high: usize) -> usize {
     if low >= high {
@@ -55,6 +65,39 @@ fn compute_opt_rec(a: &[usize], n: usize, low: usize, high: usize) -> usize {
 }
 
 #[requires(a.len() == n)]
+#[requires(sorted_rev(a))]
 fn compute_opt(a: &[usize], n: usize) -> usize {
     compute_opt_rec(a, n, 0, n)
+}
+
+// #[requires(is_h_idx(a, h))]
+#[requires(sorted_rev(a))]
+#[requires(i < a.len())]
+#[requires(x == a[i])]
+fn update_rec(a: &mut [usize], h: usize, i: usize, low: usize, high: usize, x: usize) -> usize {
+    if low < high {
+        let mid = low + (high - low) / 2;
+        if a[mid] == x {
+            update_rec(a, h, i, low, mid, x)
+        } else {
+            update_rec(a, h, i, mid + 1, high, x)
+        }
+    } else {
+        a[low] += 1;
+        if low == h && a[low] == h + 1 {
+            h + 1
+        } else {
+            h
+        }
+    }
+}
+
+// #[requires(is_h_idx(a, h))]
+#[requires(sorted_rev(a))]
+#[requires(i < a.len())]
+#[requires(x == a[i])] // Note we cannot do this assignment in the program since otherwise Prusti cannot verify that a hasn't changed
+fn update(a: &mut [usize], h: usize, i: usize, x: usize) -> usize {
+    let lo = 0_usize;
+    let hi = i;
+    update_rec(a, h, i, lo, hi, x)
 }
