84fe4eb8ba7692c300df7b52b6bfded6566e9a8a
Thomas Mayerl <tmayerl@student.ethz.ch>
2026-05-05 16:58:07 +0200

    CH1: Safety of compute

--- src/challenge1/mod.rs
+++ src/challenge1/mod.rs
@@ -1,33 +1,42 @@
 use prusti_contracts::*;
 
-/* 
-#[pure]
-#[ensures(i >= a.len() ==> result == 0)]
-#[ensures(i < a.len() ==> result == (if a[i] >= h { 1 } else { 0 }) + cnt_at_least(&a, h, i + 1))]
-fn cnt_at_least(a: &[usize], h: usize, i: usize) -> usize {
-    if i >= a.len() {
-        0
+// #[pure]
+// #[ensures(i >= a.len() ==> result == 0)]
+// #[ensures(i < a.len() ==> result == (if a[i] >= h { 1 } else { 0 }) + cnt_at_least(&a, h, i + 1))]
+// #[decreases(a.len() - i)]
+// fn cnt_at_least(a: &[usize], h: usize, i: usize) -> usize {
+//     if i >= a.len() {
+//         0
+//     } else {
+//         (if a[i] >= h { 1 } else { 0 }) + cnt_at_least(&a, h, i + 1)
+//     }
+// }
+
+// #[pure]
+// fn is_h_idx(a: &[usize], h: usize) -> bool {
+//     cnt_at_least(a, h, 0) >= h && !is_h_idx(a, h+1)
+// }
+
+// recursive because of Prusti loop limitations
+#[requires(n > 0)]
+#[requires(a.len() == n)]
+// #[ensures(is_h_idx(a, result))]
+fn compute_rec(a: &[usize], n: usize, curr_h: usize) -> usize {
+    assert!(a.len() == n);
+    if curr_h < n && curr_h < a[curr_h] {
+        // NOTE: This fails if a is mut
+        // assert!(a.len() == n);
+        let new_h = curr_h + 1;
+        compute_rec(a, n, new_h)
     } else {
-        (if a[0] >= h { 1 } else { 0 }) + cnt_at_least(&a, h, i + 1)
+        curr_h
     }
 }
 
-#[pure]
-fn is_h_idx(a: &[usize], h: usize) -> bool {
-    cnt_at_least(a, h, 0) >= h && !is_h_idx(a, h+1)
-}*/
-
 #[requires(n > 0)]
 #[requires(a.len() == n)]
-fn compute(a: &mut [usize], n: usize) -> usize {
-    let mut h: usize = 0;
-    while h < n && h < a[h] {
-        body_invariant!(n > 0);
-        body_invariant!(a.len() == n);
-        body_invariant!(0 <= h);
-        body_invariant!(h < n);
-        // body_invariant!(a === old(a));
-        h = h + 1;
-    }
-    h
+// #[ensures(is_h_idx(a, result))]
+// NOTE: We cannot currently verify termination since Prusti does not yet support decreases annotations
+fn compute(a: &[usize], n: usize) -> usize {
+    compute_rec(a, n, 0)
 }
